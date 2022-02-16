50ab745b3d4404f39174179ae4eff0c6f6895845
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-02-16 14:00:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ provider "aws"{
   region = "us-east-2"
 }
 module "lambda" {
-  source = "git::https://github.com/Ramana4u/test1.git?ref=main2"
+  source = "git::https://github.com/Ramana4u/final_project.git?ref=module"
     }
