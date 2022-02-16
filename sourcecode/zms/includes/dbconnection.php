8260360d8b444a90db097f844a7706c03a4c2b34
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-02-16 19:56:53 +0530

    Update dbconnection.php

--- sourcecode/zms/includes/dbconnection.php
+++ sourcecode/zms/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "zmsdb");
+$con=mysqli_connect("localhost", "admin", "ramana4u2021", "zmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
