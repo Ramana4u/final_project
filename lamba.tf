6eacfb2bbd6be85fdde26a8bf928c9ff4aa04af8
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-02-16 16:22:10 +0530

    Update lamba.tf

--- lamba.tf
+++ lamba.tf
@@ -1,8 +1,8 @@
 resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  function_name = "lambda-project"
   role          = "arn:aws:iam::697613968254:role/demo-Role"
-  handler       = "index.test"
+  handler       = "lambda_function.lambda_handler"
  source_code_hash = filebase64sha256("lambda_function_payload.zip")
  runtime = "python3.9"
  timeout = "840"
